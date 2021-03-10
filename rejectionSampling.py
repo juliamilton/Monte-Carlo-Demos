2d306cb139a5bfd3378f92aeb9a49e431667f19a
juliamilton <76234668+juliamilton@users.noreply.github.com>
2021-03-09 22:46:56 -0500

    Adding some documentation and rejection sampling updates

--- rejectionSampling.py
+++ rejectionSampling.py
@@ -32,6 +32,7 @@ Created on Mon Dec  7 17:08:40 2020
 # RUN THIS COMMAND FIRST! 
 #%run ./my_script.py
 #%matplotlib qt
+
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -43,98 +44,112 @@ import pandas as pd
 
 
 class AnimatedScatter(object):
-    """An animated scatter plot with proposals and targets using matplotlib.animations.FuncAnimation."""
+    """Draws the scatter plot as an animation using FuncAnimation"""
 
-    def __init__(self, numpoints, targetFunction, proposalFunction, k, domain):
+    def __init__(self, numpoints, targetFunction, proposalFunction, k, domain, ymax):
         self.k = k
         self.numpoints = numpoints+2
         self.targetFunction = targetFunction
         self.proposalFunction = proposalFunction
         self.domain = domain
+        self.ymax = ymax
         self.stream = self.data_stream()
 
         # Setup the figure and axes
         self.fig, self.ax = plt.subplots()
         self.textvar = self.ax.text(0.05, 0.95, "Init", transform=self.ax.transAxes, fontsize=10, verticalalignment='top')
+        
         # Setup FuncAnimation.
         self.ani = animation.FuncAnimation(self.fig, self.update, interval=1, 
                                           init_func=self.setup_plot, blit=False, cache_frame_data=False, save_count=self.numpoints)
 
     def setup_plot(self):
-        """Initial drawing of the scatter plot."""
+        """Initial drawing of the scatterplot"""
 
         x, y, s, c = next(self.stream).T
         self.scat = self.ax.scatter(x, y, c=c, s=s, vmin=0, vmax=1,
                                     cmap="Set3", edgecolor="none")
-        self.ax.axis([min(self.domain), max(self.domain), 0, 0.5*self.k])
+        self.ax.axis([min(self.domain), max(self.domain), 0, self.ymax])
 
         #Draw lines showing the target and proposal distributions
         line = self.ax.plot(self.domain, self.targetFunction(self.domain), 
                                     'y:', label = "Target Distribution")
-        ine2 = self.ax.plot(self.domain, k*self.proposalFunction(self.domain), 
+        line2 = self.ax.plot(self.domain, k*self.proposalFunction(self.domain), 
                                     'b:', label = "Proposal Distribution")
 
         #Add legend and title
         self.ax.legend( loc = 'upper right', prop={'size': 6}, borderpad=1)
-        self.fig.suptitle('Rejection Sampling \n Using proposal draws from a scaled Gaussian distribution', fontsize=10)
+        self.fig.suptitle('Rejection Sampling \n Using proposal draws from a scaled distribution', fontsize=10)
         
-        # FuncAnimation expects a sequence of artists in return, leave the comma in 
         return self.scat,
     
 
     def data_stream(self):
         """Generate data corresponding to selections from the target distribution and the proposal"""        
-        
-        Z = np.random.normal(0, 1, self.numpoints)
-        U = np.random.uniform(0, 1, self.numpoints)*self.k*self.proposalFunction(Z)
-        
-        Z = np.array(Z)
-        U = np.array(U)
-        
-        xyFull = np.column_stack((Z,U))
-        
+
+        #initialize
+        self.acceptedSamples = []
+        self.acceptPx = []
+        self.acceptX = []
+        self.s = []
+        self.ExArray = []
         
         j=1
+
+        #run rejection sampling as many times as the user has defined
         while j < self.numpoints+1:
             
             def rejection_sampling(iter):
-                acceptedSamples = []
-                acceptPx = []
-                acceptX = []
-                s = []
                 
-                for i in range(j):
+                #sample from f(x), which is the scaled gaussian N(0,1)
+                proposalPoint = np.random.normal(0,1)
+
+                #calculate probability that the proposal point came from the target function
+                probProposal = self.targetFunction(proposalPoint)
+
+                #calculate probability ratio
+                alpha = probProposal/(self.k*self.proposalFunction(proposalPoint))
+
+                #generate a uniform random nunmber between 0 and 1
+                u = np.random.uniform()
+
+                #accept the point if u<alpha
+                if u <= alpha:
+                    #if the point is accepted, add it to the list to find the expected value
+                    self.ExArray.append(proposalPoint)
+
+                    #the rest of this is to plot
+                    self.acceptedSamples.append(9) #marking samples as accepted or rejected to change color
+                    self.s.append(15)
+                    self.acceptPx.append(u*self.k*self.proposalFunction(proposalPoint))
+                    self.acceptX.append(proposalPoint)
                     
-                    xy = xyFull[:int(i+1),:]
-                    p_Z = self.targetFunction(Z[i])
-                    
-                    if U[i] <= p_Z:
-                        acceptedSamples.append(9) #marking samples as accepted or rejected to change color
-                        s.append(15)
-                        acceptPx.append(U[i])
-                        acceptX.append(xyFull[i,0])
-                    else:
-                        acceptedSamples.append(0)
-                        s.append(15)
+                else:
+                    # the rest of this is to plot
+                    self.acceptedSamples.append(0)
+                    self.s.append(15)
+                    self.acceptPx.append(u*self.k*self.proposalFunction(proposalPoint))
+                    self.acceptX.append(proposalPoint)
                 
-                return [xy, acceptedSamples, s, acceptPx, acceptX]
+                return [self.acceptedSamples, self.s, self.acceptPx, self.acceptX]
             
-            [xy, c, s, acceptPx, acceptX] = rejection_sampling(1)
+            [c, s, acceptPx, acceptX] = rejection_sampling(1)
             
             s = np.array(s)
             c = np.array(c)
-            xy = np.array(xy)
-            #PEstArray = np.array(acceptPx)
-            XEstArray = np.array(acceptX)
-            PEstArray = np.square(XEstArray) #for testing, change this
-            meanXEst2Array = np.mean(np.square(XEstArray))
+            y = np.array(acceptPx)
+            x = np.array(acceptX)
+ 
 
-            self.meanPEst = np.mean(XEstArray)
-            self.varPEst = np.var(PEstArray)/len(PEstArray)
+            #for calculating expected value
+            self.Ex = np.array(self.ExArray)
+            self.meanPEst = np.mean(self.Ex)         #calculate current expected value
+            self.varPEst = np.var(self.Ex)/len(self.Ex)         #calculate current variance measure
             
-   
+
             j = j+1
-            yield np.c_[xy[:,0], xy[:,1], s, c]
+
+            yield np.c_[x[:int(j-1)], y[:int(j-1)], s[:int(j-1)], c[:int(j-1)]]
             
     
 
@@ -161,29 +176,43 @@ class AnimatedScatter(object):
 
 if __name__ == '__main__':
     
-    #user should define the following
-    
+    """user should define the following section"""
+
+    # p(x) is the target distribution
     def p(x):
-            #return np.exp(-x**2/2)*(np.sin(6*x)**2 + 3*np.cos(x)**2.*np.sin(4*x)**2+1)
             return 0.2*st.norm.pdf(x, loc=1, scale=0.3)+0.7*st.norm.pdf(x, loc=-0.5, scale=0.4) #Gaussian mixture
             
-            
-    def q(x):
+            # uncomment below and comment out the above line to see an alternative example
+            #return np.exp(-x**2/2)*(np.sin(6*x)**2 + 3*np.cos(x)**2.*np.sin(4*x)**2+1)
+    
+    # f(x) is the proposal distribution        
+    def f(x):
         return st.norm.pdf(x, loc=0, scale=1)
     
+    #change x and y axes below if necessary
     xmin = -4
     xmax = 4
+    ymax = 1
     
-    #end user defined
+    #change number of runs below (recommended minimum 1000)
+    MCruns = 3000
+    
+    """end user defined"""
+
     
     domain = np.linspace(xmin, xmax,10000, endpoint=True)
-    k = max(p(domain)/q(domain))
 
-    # Set up formatting for the movie files - uncomment this if you want to save it as a movie
+    # k is the scaling factor for the proposal distribution
+    k = max(p(domain)/f(domain))
 
+
+    """Uncomment the two lines below and the last line if you would like to save the animation as a movie file """
     #Writer = animation.writers['ffmpeg']
-    #writer = Writer(fps =200, metadata=dict(artist='Me'))
+    #writer = Writer(fps =120, metadata=dict(artist='Me'))
         
-    a = AnimatedScatter(1003, p, q, k, domain)
+    a = AnimatedScatter(MCruns, p, f, k, domain, ymax)
     plt.show()
-    #a.ani.save('rejectionSamp1.mp4', writer=writer)
+
+
+    """Uncomment the line below if you would like to save the animation as a movie file """
+    #a.ani.save('MCMC3.mp4', writer=writer)
